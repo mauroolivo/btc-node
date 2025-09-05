00324f90c26fa284378830faaa3f8920ddb93c17
Mauro Olivo <olivomauro@gmail.com>
2025-09-05 21:50:35 +0200

    Client/server separation

---
+++ src/app/blockchain2/page.tsx
@@ -0,0 +1,24 @@
+'use server'
+import Client from "@/app/blockchain2/client";
+import {getmempoolinfo, getRawmempool} from "@/api/api";
+import {RawmempoolTx} from "@/models/mempool";
+
+export default async function Page() {
+
+  const mempoolinfo = await getmempoolinfo()
+  const rawMempool = await getRawmempool(true)
+  const data: [string: RawmempoolTx] = rawMempool.result as [string: RawmempoolTx];
+  const temp_txs: RawmempoolTx[] = []
+  Object.entries(data).forEach(([k, v]) => {
+    v.txid_key = k
+    temp_txs.push(v)
+  })
+  temp_txs.sort((a, b) => b.time - a.time);
+
+  // await new Promise(resolve => setTimeout(resolve, 5000));
+  return (
+    <>
+      <Client mempoolinfo={mempoolinfo} txs={temp_txs} />
+    </>
+  );
+}
