9d4b107f664e6a9360cee81b1e8d4604a14553ac
Mauro Olivo <olivomauro@gmail.com>
2025-09-20 14:36:03 +0200

    refactor tabbar navigation

--- src/app/wallet/[[...segmentName]]/wallet-home.tsx
+++ src/app/wallet/[[...segmentName]]/wallet-home.tsx
@@ -1,32 +1,49 @@
 "use client";
 
-import {TabItem, Tabs} from "flowbite-react";
+import {Button, TabItem, Tabs} from "flowbite-react";
 import WalletAddresses from "@/app/wallet/[[...segmentName]]/wallet-addresses";
 import WalletDescriptors from "@/app/wallet/[[...segmentName]]/wallet-descriptors";
 import WalletTxsList from "@/app/wallet/[[...segmentName]]/wallet-txs";
 import WalletUTXOs from "@/app/wallet/[[...segmentName]]/wallet-utxos";
 import WalletInfo from "@/app/wallet/[[...segmentName]]/wallet-info";
+import React from "react";
 export default function WalletHome() {
+
+  enum Tab {
+    INFO,
+    TRANSACTION,
+    UNSPENT,
+    ADDRESS,
+    DESCRIPTOR
+  }
+  const [current, setCurrent] = React.useState(Tab.INFO);
 
   return (
     <>
-      <Tabs aria-label="Default tabs" variant="default" className="custom-tabs">
-        <TabItem title="Wallet info">
-          <WalletInfo />
-        </TabItem>
-        <TabItem title="Transactions" >
-          <WalletTxsList />
-        </TabItem>
-        <TabItem title="UNSPENT" >
-          <WalletUTXOs />
-        </TabItem>
-        <TabItem title="Addresses">
-          <WalletAddresses />
-        </TabItem>
-        <TabItem title="Descriptors">
-          <WalletDescriptors />
-        </TabItem>
-      </Tabs>
+
+      <div className={" flex flex-wrap gap-8 uppercase justify-end"}>
+        <div className={""} onClick={() => {setCurrent(Tab.INFO)}}>Wallet info</div>
+        <div className={""} onClick={() => {setCurrent(Tab.TRANSACTION)}}>Transactions</div>
+        <div className={""} onClick={() => {setCurrent(Tab.UNSPENT)}}>UTXO</div>
+        <div className={""} onClick={() => {setCurrent(Tab.ADDRESS)}}>Addresses</div>
+        <div className={""} onClick={() => {setCurrent(Tab.DESCRIPTOR)}}>Descriptor</div>
+      </div>
+      {
+        current == Tab.INFO && <WalletInfo />
+      }
+      {
+        current == Tab.TRANSACTION && <WalletTxsList />
+      }
+      {
+        current == Tab.UNSPENT && <WalletUTXOs />
+      }
+      {
+        current == Tab.ADDRESS && <WalletAddresses />
+      }
+      {
+        current == Tab.DESCRIPTOR && <WalletDescriptors />
+      }
+
     </>
   );
 }
